4f90d278b483ede61b0e7253a6cc4a7a07948bef
yusukec <infinite20yc@gmail.com>
2020-12-12 17:17:14 +0900

    Added working list tags endpoint.

---
+++ app/recipe/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+
+router = DefaultRouter()  # DRFの機能。自動でURLを割り当てる。
+router.register('tags', views.TagViewSet)
+# 何が嬉しいかというと、ViewSet側でURLを追加して行ったときに、
+# したのurlpatternsを自動で更新してくれる。
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls))
+]
