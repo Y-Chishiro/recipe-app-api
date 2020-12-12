83571e2ea7f7b7b6d2193842ef8cd84eeab594f5
yusukec <infinite20yc@gmail.com>
2020-12-12 17:48:22 +0900

    Added list ingredients feature.

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -8,6 +8,7 @@ router = DefaultRouter()  # DRFの機能。自動でURLを割り当てる。
 router.register('tags', views.TagViewSet)
 # 何が嬉しいかというと、ViewSet側でURLを追加して行ったときに、
 # したのurlpatternsを自動で更新してくれる。
+router.register('ingredients', views.IngredientViewSet)
 
 app_name = 'recipe'
 
